4ab88ca0f438462bdef99985009d0fc1eaee5612
laptop <the.red.lemon@yandex.ru>
2023-06-10 23:48:24 +0300

    add routes getAllMovies and createMovie

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,8 +1,52 @@
 const movieSchema = require('../models/movie');
 
+const {
+  NotFoundError,
+} = require('../utils/error');
+
 module.exports.getAllMovies = (req, res, next) => {
   movieSchema
     .find({})
     .then((cards) => res.send(cards.reverse()))
     .catch(next);
+};
+
+module.exports.createMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+  } = req.body;
+
+  movieSchema
+    .create({
+      country,
+      director,
+      duration,
+      year,
+      description,
+      image,
+      trailerLink,
+      thumbnail,
+      owner: req.user._id,
+      movieId,
+      nameRU,
+      nameEN,
+    })
+    .then((card) => res.send(card))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(new NotFoundError('Invalid data when post card'));
+      }
+
+      return next(err);
+    });
 };
--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,16 @@
 const movieRouter = require('express').Router();
 
 const {
+  validateCreateCard,
+} = require('../middlewares/celebrate');
+
+const {
   getAllMovies,
+  createMovie,
 } = require('../controllers/movie');
 
 movieRouter.get('/', getAllMovies);
+// movieRouter.post('/', validateCreateCard, createMovie);
+movieRouter.post('/', createMovie);
 
 module.exports = movieRouter;
