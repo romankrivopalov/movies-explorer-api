20c753d9fd592c31730c160371a61bd46f19b1a8
laptop <the.red.lemon@yandex.ru>
2023-06-11 11:10:17 +0300

    create login and create user controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,86 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const userSchema = require('../models/users');
 
 const {
   NotFoundError,
+  UnathorizedError,
   BadRequestError,
+  ConflictError,
 } = require('../utils/error');
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return userSchema
+    .findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return next(new UnathorizedError('incorrect email or password'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return next(new UnathorizedError('incorrect email or password'));
+          }
+
+          const token = jwt.sign(
+            { _id: user._id },
+            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+            { expiresIn: '7d' },
+          );
+          res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true });
+
+          return res
+            .status(200)
+            .send({
+              _id: user._id,
+              name: user.name,
+              email: user.email,
+            });
+        });
+    })
+    .catch(next);
+};
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    name,
+    about,
+    avatar,
+    email,
+    password,
+  } = req.body;
+
+  bcrypt
+    .hash(password, 10)
+    .then((hash) => userSchema.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
+    .then((user) => {
+      const userObj = user.toObject();
+      delete userObj.password;
+      res.status(201).send(userObj);
+    })
+    .catch((err) => {
+      if (err.code === 11000) {
+        return next(new ConflictError('A user with such a email is already registered'));
+      }
+
+      if (err.name === 'ValidationError') {
+        return next(new NotFoundError('Invalid data when post user'));
+      }
+
+      return next(err);
+    });
+};
 
 module.exports.getUserById = (req, res, next) => {
   const userId = req.params.id;
